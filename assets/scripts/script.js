dca5162951f7923f8c7e3bc517834482f170504e
ikanurfitriani <ikan07393@gmail.com>
2024-05-14 17:23:00 +0700

    Update style.css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,9 +18,30 @@ document.addEventListener('DOMContentLoaded', function() {
             item.classList.add('active');
         }
     });
+
+    // Pencegahan klik kanan untuk melindungi isi konten
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
 });
 
-// Pencegahan klik kanan untuk melindungi isi konten
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
+// Definisikan variabel untuk menyimpan posisi scroll sebelumnya
+let lastScrollTop = 0;
+
+// Tambahkan event listener untuk event scroll
+window.addEventListener("scroll", function() {
+    // Ambil posisi scroll saat ini
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    // Bandingkan posisi scroll saat ini dengan posisi scroll sebelumnya
+    if (currentScroll > lastScrollTop) {
+        // Jika scroll ke bawah, sembunyikan navbar dengan transformasi translateY(-100%)
+        document.querySelector("nav").style.transform = "translateY(-100%)";
+    } else {
+        // Jika scroll ke atas, tampilkan kembali navbar dengan transformasi translateY(0)
+        document.querySelector("nav").style.transform = "translateY(0)";
+    }
+    
+    // Simpan posisi scroll saat ini sebagai posisi scroll sebelumnya
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
